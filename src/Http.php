c2383c77fc2bd635a290725b68757eb0b30a50b0
SmetDenis <denis@jbzoo.com>
2016-01-15 17:37:36 +0600

    getHeaders function

--- src/Http.php
+++ src/Http.php
@@ -138,4 +138,93 @@ class Http
         return false;
         // @codeCoverageIgnoreEnd
     }
+
+    /**
+     * Get all HTTP headers
+     * @see https://github.com/symfony/http-foundation/blob/master/ServerBag.php
+     *
+     * @return array
+     *
+     * @SuppressWarnings(PHPMD.Superglobals)
+     */
+    public static function getHeaders()
+    {
+        $headers = array();
+
+        $contentHeaders = array('CONTENT_LENGTH' => true, 'CONTENT_MD5' => true, 'CONTENT_TYPE' => true);
+
+        foreach ($_SERVER as $key => $value) {
+            if (0 === strpos($key, 'HTTP_')) {
+                $headers[substr($key, 5)] = $value;
+            } // CONTENT_* are not prefixed with HTTP_
+            elseif (isset($contentHeaders[$key])) {
+                $headers[$key] = $value;
+            }
+        }
+
+        if (Vars::get($_SERVER['PHP_AUTH_USER'])) {
+            $headers['PHP_AUTH_USER'] = $_SERVER['PHP_AUTH_USER'];
+            $headers['PHP_AUTH_PW']   = Vars::get($_SERVER['PHP_AUTH_PW'], '');
+
+        } else {
+            /*
+             * php-cgi under Apache does not pass HTTP Basic user/pass to PHP by default
+             * For this workaround to work, add these lines to your .htaccess file:
+             * RewriteCond %{HTTP:Authorization} ^(.+)$
+             * RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+             *
+             * A sample .htaccess file:
+             * RewriteEngine On
+             * RewriteCond %{HTTP:Authorization} ^(.+)$
+             * RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+             * RewriteCond %{REQUEST_FILENAME} !-f
+             * RewriteRule ^(.*)$ app.php [QSA,L]
+             */
+
+            $authorizationHeader = null;
+            if (Vars::get($_SERVER['HTTP_AUTHORIZATION'])) {
+                $authorizationHeader = $_SERVER['HTTP_AUTHORIZATION'];
+
+            } elseif (Vars::get($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+                $authorizationHeader = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+            }
+
+            if (null !== $authorizationHeader) {
+                if (0 === stripos($authorizationHeader, 'basic ')) {
+                    // Decode AUTHORIZATION header into PHP_AUTH_USER
+                    // And PHP_AUTH_PW when authorization header is basic
+                    $exploded = explode(':', base64_decode(substr($authorizationHeader, 6)), 2);
+
+                    if (count($exploded) == 2) {
+                        list($headers['PHP_AUTH_USER'], $headers['PHP_AUTH_PW']) = $exploded;
+                    }
+
+                } elseif (empty($_SERVER['PHP_AUTH_DIGEST']) && (0 === stripos($authorizationHeader, 'digest '))) {
+                    // In some circumstances PHP_AUTH_DIGEST needs to be set
+                    $headers['PHP_AUTH_DIGEST'] = $authorizationHeader;
+                    $_SERVER['PHP_AUTH_DIGEST'] = $authorizationHeader;
+
+                } elseif (0 === stripos($authorizationHeader, 'bearer ')) {
+                    /*
+                     * XXX: Since there is no PHP_AUTH_BEARER in PHP predefined variables,
+                     *      I'll just set $headers['AUTHORIZATION'] here.
+                     *      http://php.net/manual/en/reserved.variables.server.php
+                     */
+                    $headers['AUTHORIZATION'] = $authorizationHeader;
+                }
+            }
+        }
+
+        // PHP_AUTH_USER/PHP_AUTH_PW
+        if (Vars::get($headers['PHP_AUTH_USER'])) {
+            $authorization = 'Basic ' . base64_encode($headers['PHP_AUTH_USER'] . ':' . $headers['PHP_AUTH_PW']);
+
+            $headers['AUTHORIZATION'] = $authorization;
+
+        } elseif (Vars::get($headers['PHP_AUTH_DIGEST'])) {
+            $headers['AUTHORIZATION'] = $headers['PHP_AUTH_DIGEST'];
+        }
+
+        return $headers;
+    }
 }
