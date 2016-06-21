06bafbc61dd428c50500953a4c0e3e51a60da212
smetdenis <admin@jbzoo.com>
2016-06-21 13:51:47 +0700

    Add helper for ob_* functions

---
+++ src/Ob.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * JBZoo Utils
+ *
+ * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package   Utils
+ * @license   MIT
+ * @copyright Copyright (C) JBZoo.com,  All rights reserved.
+ * @link      https://github.com/JBZoo/Utils
+ * @author    Denis Smetannikov <denis@jbzoo.com>
+ */
+
+namespace JBZoo\Utils;
+
+/**
+ * Class Ob
+ * @package JBZoo\Utils
+ */
+class Ob
+{
+    /**
+     * Clean ll ob_* buffers
+     */
+    public static function clean()
+    {
+        while (@ob_end_clean()) {
+            // noop
+        }
+    }
+}
